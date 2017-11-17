592f7cb41f6695b1ff810533ded442922a81310a
ALE <ALE@DESKTOP-L9VI806>
2017-11-17 13:32:01 +0000

    Kata5 version 1

---
+++ src/kata5/main/Kata5.java
@@ -0,0 +1,33 @@
+
+package kata5.main;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class Kata5 {
+
+
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException, SQLException {
+        
+        Class.forName("org.sqlite.JDBC");
+        Connection con = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\ALE\\Documents\\NetBeansProjectsm\\Kata5P1\\KATA5.DB");	
+        
+        Statement st= con.createStatement();
+        
+        String query = "SELECT * FROM PEOPLE";
+        ResultSet rs = st.executeQuery(query);
+        while(rs.next()){
+            System.out.println(rs.getInt(1));
+            System.out.println(rs.getString(2));
+        }
+        
+        
+    }
+}
